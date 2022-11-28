04df42d60d0c84b8de17094cb4b307e25075a1e2
snerva <selenenerva@live.it>
2022-11-28 12:25:51 +0100

    snack1

---
+++ snack_1/index.php
@@ -0,0 +1,31 @@
+<?php
+//Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema.
+
+$partite = [
+    ['olimpia', 'milano', '54', '70'],
+    ['bari', 'roma', '78', '35']
+];
+
+foreach ($partite as $item) {
+    foreach ($item as $items) {
+        echo $items . ' ';
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
